d60966650e9fe17ac4a5ff40805a08a0662aa70f
王航 <wanghang@666.com>
2017-09-17 21:28:32 +0800

    集成新浪、微信、qq分享的SDK

---
+++ HBShare/HBShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBShare
+//
+//  Created by Patrick W on 2017/9/17.
+//  Copyright © 2017年 Original. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBShare/HBShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBShare
+//
+//  Created by Patrick W on 2017/9/17.
+//  Copyright © 2017年 Original. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
